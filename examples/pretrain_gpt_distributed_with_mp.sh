ddbea8f6432db139f0b8ce3805f88c2a223d5a3b
haytham918 <yunxuant@umich.edu>
2023-11-16 00:05:15 -0500

    Change train iteration number to 2000

--- examples/pretrain_gpt_distributed_with_mp.sh
+++ examples/pretrain_gpt_distributed_with_mp.sh
@@ -37,7 +37,7 @@ GPT_ARGS="
     --micro-batch-size 4 \
     --global-batch-size 16 \
     --lr 0.00015 \
-    --train-iters 500000 \
+    --train-iters 2000 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
